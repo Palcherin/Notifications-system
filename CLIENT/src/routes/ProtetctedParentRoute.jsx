10bd6cbb5561a428c7424397526226f40ebea958
Jobwafula <jobwafulabg@gmail.com>
2024-02-18 19:24:22 +0300

    parent route

--- CLIENT/src/routes/ProtetctedParentRoute.jsx
+++ CLIENT/src/routes/ProtetctedParentRoute.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import {Navigate, useNavigate,Outlet,useLocation,Redirect,Route} from 'react-router-dom'
 export default function ProtetctedParentRoute() {
-  return (
-    <div>ProtetctedParentRoute</div>
-  )
+  // fetch token from local storage,,not set yet
+  const token = localStorage.getItem("token")
+
+  const navigate = useNavigate()
+  // function to return to previous route
+  function PresentPage (){
+    navigate(-1)
+    
+  }
+
+  if(!token) return <Navigate  to='/' />
+
+  useEffect(()=>{
+    if(token && jwtDecode(token).role!==parent){
+      PresentPage()
+    }
+  },[token && jwtDecode(token).role!==parent])
+
+  const decodedData = jwtDecode(token);
+
+
+  if (decodedData.role === "parent") {
+    return <Outlet {...props} />;
+  }
+ else if(decodedData.role!=="parent"){
+   PresentPage()
+  }
+
+
 }
