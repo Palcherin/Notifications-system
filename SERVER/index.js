86c80a96c63e6b9ffa48f36b28c285e6d9fa97d8
Jobwafula <jobwafulabg@gmail.com>
2024-04-07 20:18:15 +0300

    socket

--- SERVER/index.js
+++ SERVER/index.js
@@ -1,69 +1,69 @@
-const express = require("express");
-const app = express();
-const router = express.Router();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
-
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User connected ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(5050, () => {
-  console.log('SERVER IS RUNNING');
-});
-
-
-// const express=require('express');
-// const app=express();
-// const http=require("http");
-// const {Server} =require("socket.io");
-// const cors=require("cors")
-
+// const express = require("express");
+// const app = express();
+// const router = express.Router();
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
 // app.use(cors());
 
+// const server = http.createServer(app);
 
-// const server=http.createServer(app)
-
-// const io=new Server(server,{
-//     cors:{
-//         origin:"http://localhost:5073",
-//         method:["GET","POST"],
-//     }
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//   },
 // });
 
-// io.on("connection", (socket)=>{
-//     console.log(`user connected: ${socket.id}`);
-//     socket.on("send_message", (data)=>{
-//         socket.broadcast.emit("receive_message", data)
-//     })
-//     socket.on("disconnected",()=>{
-//         console.log("user disconnected", socket.id)
-//     } )
-// })
+// io.on("connection", (socket) => {
+//   console.log(`User connected ${socket.id}`);
 
-// server.listen(3004,() =>{
-//     console.log("SERVER is running");
-// })
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+// });
+
+// server.listen(5050, () => {
+//   console.log('SERVER IS RUNNING');
+// });
+
+
+// // const express=require('express');
+// // const app=express();
+// // const http=require("http");
+// // const {Server} =require("socket.io");
+// // const cors=require("cors")
+
+// // app.use(cors());
+
+
+// // const server=http.createServer(app)
+
+// // const io=new Server(server,{
+// //     cors:{
+// //         origin:"http://localhost:5073",
+// //         method:["GET","POST"],
+// //     }
+// // });
+
+// // io.on("connection", (socket)=>{
+// //     console.log(`user connected: ${socket.id}`);
+// //     socket.on("send_message", (data)=>{
+// //         socket.broadcast.emit("receive_message", data)
+// //     })
+// //     socket.on("disconnected",()=>{
+// //         console.log("user disconnected", socket.id)
+// //     } )
+// // })
+
+// // server.listen(3004,() =>{
+// //     console.log("SERVER is running");
+// // })
--- SERVER/server.js
+++ SERVER/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const socketIO = require('socket.io');
+const http = require('http');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -8,6 +10,9 @@ const mongoose = require('mongoose');
 const auth = require('./routes/auth');
 const twilio=require('./routes/Twilio');
 const socket=require('./routes/Socket')
+const server = http.createServer(app);
+const io = socketIO(server);
+const Message = require('./models/Message');
 
 const PORT = process.env.PORT || 3000;
 
@@ -32,6 +37,36 @@ async function connect() {
 
 connect();
 
+
+// socket
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  // Listen for incoming chat messages
+  socket.on('chat message', (data) => {
+    console.log('Received message:', data);
+
+    // Save the message to MongoDB
+    const message = new Message({ user: data.user, text: data.message });
+    message.save((err) => {
+      if (err) {
+        console.error('Error saving message to database:', err);
+      } else {
+        console.log('Message saved to the database');
+      }
+    });
+
+    // Broadcast the message to all connected clients
+    io.emit('chat message', data);
+  });
+
+  // Listen for user disconnection
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+
 app.get('/', (req, res) => {
   const status = {
     "status": "running"
@@ -42,8 +77,8 @@ app.get('/', (req, res) => {
 
 app.use('/api', auth);
 app.use('/api',twilio);
-app.use('/api', socket);
 
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 });
